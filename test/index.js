8c91a4746879ad2e7de0262a4c0419c8f1692a5d
Ant Cosentino <antony@mycosentino.com>
2017-06-12 10:10:23 +0200

    fix test case

--- test/index.js
+++ test/index.js
@@ -6,28 +6,19 @@ var {expect} = require('chai')
 var gore = require('..')
 
 describe('gore', function() {
-  
   describe('instance', function() {
-    
-    // shared instance
-    var g
-    
-    it('should set up cleanly', function(done) {
+    it('should set up and tear down cleanly', function(done) {
       gore().then(function(gore) {
-        g = gore
-        expect(gore).have.keys([
+        expect(gore).to.have.keys([
           'input', 'page',
           'dom', 'network',
           'suite', 'runtime',
           'kill', 'sleep'
         ])
-        done()
-      }).catch(done)
+        return gore.kill()
+      }).then(
+        done.bind(done, null)
+      ).catch(done)
     })
-
-    it('should tear down cleanly', function(done) {
-      g.kill().then(done.bind(done, null)).catch(done)
-    })
-
   })
 })
